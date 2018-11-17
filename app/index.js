9665899fee39a28dd88fce31f9788a80ab110771
爬上上坡 <pashangshangpo@gmail.com>
2018-11-17 13:26:14 +0800

    添加选择包管理器提问

--- app/index.js
+++ app/index.js
@@ -8,6 +8,16 @@ module.exports = class extends Generator {
         name: 'name',
         message: '请输入项目名',
         default: this.appname.split(' ').join('-')
+      },
+      {
+        type: 'list',
+        name: 'packageManage',
+        message: '请选择你的包管理器',
+        choices: [
+          'npm',
+          'yarn'
+        ],
+        default: 0
       }
     ]).then(config => {
       console.log(config)
