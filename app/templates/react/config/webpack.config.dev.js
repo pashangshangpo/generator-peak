95b2ea2775336437ecfff8fb86b1df4ec0f6ef43
爬上上坡 <pashangshangpo@gmail.com>
2018-11-22 16:50:51 +0800

    去除未使用到的方法

--- app/templates/react/config/webpack.config.dev.js
+++ app/templates/react/config/webpack.config.dev.js
@@ -1,13 +1,8 @@
 
-const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 
 const common = require('./webpack.config.common')
-
-const resolve = pathName => {
-  return path.resolve('.', pathName)
-}
 
 module.exports = merge(common, {
   mode: 'development',
