00fcf861a9c1fffe8e25e04530fc3f8529b0b570
爬上上坡 <pashangshangpo@gmail.com>
2018-11-23 19:51:29 +0800

    添加公共代码接口

--- app/templates/react/peak.config.js
+++ app/templates/react/peak.config.js
@@ -8,5 +8,6 @@ module.exports = {
   webpackConfigDev: 'config/webpack.config.dev.js',
   webpackConfigProd: 'config/webpack.config.prod.js',
   template: 'index.html',
-  publicPath: '/public'
+  publicPath: '/public',
+  commonCode: []
 }
